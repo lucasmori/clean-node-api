21ff2bf9b33c0e6da9a28841e43a16c3dbc7ebae
Lucas Mori <lucastecnew@gmail.com>
2022-05-22 18:31:46 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,9 +2,10 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
-
+  uri: null as string,
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
       // @ts-expect-error
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -12,11 +13,16 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null
   },
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   },
-  merge (insertedId, data): any {
-    return Object.assign({}, data, { id: insertedId.toString() })
+  map (data: any) {
+    const { _id, ...rest } = data
+    return { ...rest, id: _id.toHexString() }
   }
 }
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,5 +1,6 @@
 import { Express, Router } from 'express'
 import fg from 'fast-glob'
+
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
