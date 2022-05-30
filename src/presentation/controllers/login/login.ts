f2fcf8114c898982a2e11276081c4770f021502b
Lucas Mori <lucastecnew@gmail.com>
2022-05-29 22:13:37 -0300

    feat: refactor and add composite pattern

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,43 @@
+import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './login-protocols'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
+import { InvalidParamError, MissingParamError } from '../../errors'
+
+export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+  private readonly authentication: Authentication;
+
+  constructor (emailValidator: EmailValidator, authentication: Authentication) {
+    this.emailValidator = emailValidator
+    this.authentication = authentication
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['email', 'password']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      const { email, password } = httpRequest.body
+
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      const accessToken = await this.authentication.auth(email, password)
+
+      if (!accessToken) {
+        return unauthorized()
+      }
+
+      return ok({ accessToken })
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from '../../errors'
+
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('any_field')
+}
+
+describe('RequiredField Validation', () => {
+  it('Should return a MissingParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  it('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any_field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
