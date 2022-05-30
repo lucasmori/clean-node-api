525f895e1949390f5aa01025f2dfc11e7142d09b
Lucas Mori <lucastecnew@gmail.com>
2022-05-29 22:48:50 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,6 +1,6 @@
-import { HttpResponse } from '../protocols'
-import { ServerError } from '../errors'
-import { UnauthorizedError } from '../errors/unauthorized-error'
+import { HttpResponse } from '../../protocols'
+import { ServerError } from '../../errors'
+import { UnauthorizedError } from '../../errors/unauthorized-error'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
